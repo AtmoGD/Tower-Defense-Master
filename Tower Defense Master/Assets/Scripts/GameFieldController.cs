c775e114fdc8f125582d572b4498d0ae2dc07cde
Dennis <dennishawran@gmail.com>
2021-01-29 10:09:56 +0100

    Try

---
+++ Tower Defense Master/Assets/Scripts/GameFieldController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldController : MonoBehaviour
+{
+    [SerializeField]
+    private int gameFieldSize = 5;
+
+    [SerializeField]
+    private GameObject gameFieldGround = null;
+
+    [SerializeField]
+    private GameObject gameFieldSelectedPrefab = null;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
